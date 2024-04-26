5a41b3c7c8a30dd34f2c277de46fc8876763d42e
Melih Hakan Pektas <contact@melihhakanpektas.com>
2024-04-26 12:43:44 +0300

    logo2 update

--- lib/src/logo2.dart
+++ lib/src/logo2.dart
@@ -93,22 +93,44 @@ class _Logo2State extends State<Logo2> {
                             // Get resolution of the logo image
                             final logoImageSize = Size(logoImageValue.image.width.toDouble(),
                                 logoImageValue.image.height.toDouble());
+                            final logoRenderSize = Size(logoImageSize.width * tshirtCurrentScale,
+                                logoImageSize.height * tshirtCurrentScale);
+
                             // Update the controller value
                             if (tshirtPreviousScale != tshirtCurrentScale) {
-                              widget._controller.value = controllerValue.copyWith(
-                                tshirtScale: tshirtCurrentScale,
-                                tshirtImageSize: tshirtImageSize,
-                                logoImageSize: logoImageSize,
-                                tshirtGlobalPosition: tshirtGlobalPosition,
-                                logoPositionOffset: Offset(
-                                  controllerValue.logoPositionOffset.dx *
-                                      tshirtCurrentScale /
-                                      tshirtPreviousScale,
-                                  controllerValue.logoPositionOffset.dy *
-                                      tshirtCurrentScale /
-                                      tshirtPreviousScale,
-                                ),
-                              );
+                              if (controllerValue.logoPositionOffset == Offset.zero) {
+                                widget._controller.value = controllerValue.copyWith(
+                                  tshirtScale: tshirtCurrentScale,
+                                  tshirtImageSize: tshirtImageSize,
+                                  logoImageSize: logoImageSize,
+                                  tshirtGlobalPosition: tshirtGlobalPosition,
+                                  logoPositionOffset: Offset(
+                                    (controllerValue.logoPositionOffset.dx *
+                                            tshirtCurrentScale /
+                                            tshirtPreviousScale) +
+                                        (tshirtRenderSize.width - logoRenderSize.width) / 2,
+                                    controllerValue.logoPositionOffset.dy *
+                                            tshirtCurrentScale /
+                                            tshirtPreviousScale +
+                                        (tshirtRenderSize.height - logoRenderSize.height) / 2,
+                                  ),
+                                );
+                              } else {
+                                widget._controller.value = controllerValue.copyWith(
+                                  tshirtScale: tshirtCurrentScale,
+                                  tshirtImageSize: tshirtImageSize,
+                                  logoImageSize: logoImageSize,
+                                  tshirtGlobalPosition: tshirtGlobalPosition,
+                                  logoPositionOffset: Offset(
+                                    (controllerValue.logoPositionOffset.dx *
+                                        tshirtCurrentScale /
+                                        tshirtPreviousScale),
+                                    controllerValue.logoPositionOffset.dy *
+                                        tshirtCurrentScale /
+                                        tshirtPreviousScale,
+                                  ),
+                                );
+                              }
                             }
                           });
                           return Stack(
@@ -128,6 +150,22 @@ class _Logo2State extends State<Logo2> {
                                     controllerValue.tshirtGlobalPosition.dy,
                                 child: _buildLogo(controllerValue),
                               ),
+                              Align(
+                                alignment: Alignment.center,
+                                child: Image.asset(
+                                  controllerValue.tshirtImage,
+                                  color: Colors.white.withOpacity(.9),
+                                  colorBlendMode: BlendMode.srcOut,
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                              Positioned(
+                                left: controllerValue.logoPositionOffset.dx +
+                                    controllerValue.tshirtGlobalPosition.dx,
+                                top: controllerValue.logoPositionOffset.dy +
+                                    controllerValue.tshirtGlobalPosition.dy,
+                                child: _buildLogo(controllerValue, showLogo: false),
+                              ),
                             ],
                           );
                         });
@@ -137,7 +175,7 @@ class _Logo2State extends State<Logo2> {
         });
   }
 
-  Widget _buildLogo(MockupData value) {
+  Widget _buildLogo(MockupData value, {bool showLogo = true}) {
     return ValueListenableBuilder(
         valueListenable: transformOffset,
         builder: (context, transformOffsetValue, child) {
@@ -147,64 +185,72 @@ class _Logo2State extends State<Logo2> {
               ..rotateX(transformOffsetValue.dy * pi / 180)
               ..rotateZ(transformOffsetValue.dx * pi / 180),
             alignment: Alignment.center,
-            child: Listener(
-              onPointerSignal: (event) {
-                if (event is PointerScrollEvent) {
-                  final previousImageSize = Offset(
-                      value.logoImageSize.width * value.tshirtScale * value.logoScale,
-                      value.logoImageSize.height * value.tshirtScale * value.logoScale);
-                  final newScale = (value.logoScale - event.scrollDelta.dy / 100).clamp(0.1, 10.0);
-                  final imageSize = Offset(value.logoImageSize.width * value.tshirtScale * newScale,
-                      value.logoImageSize.height * value.tshirtScale * newScale);
-                  final additionOffset = (previousImageSize - imageSize) / 2;
-                  final newOffset = value.logoPositionOffset + additionOffset;
-                  widget._controller.value = widget._controller.value.copyWith(
-                    logoScale: newScale,
-                    logoPositionOffset: newOffset,
-                  );
-                  if (event is PointerPanZoomStartEvent) {
-                    print('PointerPanZoomStartEvent');
-                  }
-                }
-              },
-              child: GestureDetector(
-                onScaleStart: (details) {
-                  dragdifference = details.focalPoint - value.logoPositionOffset;
-                  dragStarted = true;
-                  previousScale = value.logoScale;
-                },
-                onScaleUpdate: (details) {
-                  final previousImageSize = Offset(
-                      value.logoImageSize.width * value.tshirtScale * value.logoScale,
-                      value.logoImageSize.height * value.tshirtScale * value.logoScale);
-                  final newScale = previousScale * (details.scale);
-                  final newImageSize = Offset(
-                      value.logoImageSize.width * value.tshirtScale * newScale,
-                      value.logoImageSize.height * value.tshirtScale * newScale);
-                  final additionOffset = (previousImageSize - newImageSize) / 2;
-                  Offset newOffset = Offset.zero;
-                  if (newScale == value.logoScale) {
-                    newOffset = details.focalPoint - dragdifference + additionOffset;
-                  } else {
-                    newOffset = value.logoPositionOffset + additionOffset;
-                  }
-                  widget._controller.value = widget._controller.value.copyWith(
-                    logoScale: newScale,
-                    logoPositionOffset: newOffset,
-                  );
-                },
-                onScaleEnd: (details) {
-                  dragStarted = false;
-                  print('ScaleEnd');
-                },
-                child: Image.asset(
-                  value.logoImage,
-                  fit: BoxFit.contain,
-                  width: value.logoImageSize.width * value.tshirtScale * value.logoScale,
-                  height: value.logoImageSize.height * value.tshirtScale * value.logoScale,
-                ),
-              ),
-            ),
+            child: showLogo
+                ? Image.asset(
+                    value.logoImage,
+                    fit: BoxFit.contain,
+                    width: value.logoImageSize.width * value.tshirtScale * value.logoScale,
+                    height: value.logoImageSize.height * value.tshirtScale * value.logoScale,
+                  )
+                : Listener(
+                    onPointerSignal: (event) {
+                      if (event is PointerScrollEvent) {
+                        final previousImageSize = Offset(
+                            value.logoImageSize.width * value.tshirtScale * value.logoScale,
+                            value.logoImageSize.height * value.tshirtScale * value.logoScale);
+                        final newScale =
+                            (value.logoScale - event.scrollDelta.dy / 100).clamp(0.1, 10.0);
+                        final imageSize = Offset(
+                            value.logoImageSize.width * value.tshirtScale * newScale,
+                            value.logoImageSize.height * value.tshirtScale * newScale);
+                        final additionOffset = (previousImageSize - imageSize) / 2;
+                        final newOffset = value.logoPositionOffset + additionOffset;
+                        widget._controller.value = widget._controller.value.copyWith(
+                          logoScale: newScale,
+                          logoPositionOffset: newOffset,
+                        );
+                        if (event is PointerPanZoomStartEvent) {
+                          print('PointerPanZoomStartEvent');
+                        }
+                      }
+                    },
+                    child: GestureDetector(
+                      onScaleStart: (details) {
+                        dragdifference = details.focalPoint - value.logoPositionOffset;
+                        dragStarted = true;
+                        previousScale = value.logoScale;
+                      },
+                      onScaleUpdate: (details) {
+                        final previousImageSize = Offset(
+                            value.logoImageSize.width * value.tshirtScale * value.logoScale,
+                            value.logoImageSize.height * value.tshirtScale * value.logoScale);
+                        final newScale = previousScale * (details.scale);
+                        final newImageSize = Offset(
+                            value.logoImageSize.width * value.tshirtScale * newScale,
+                            value.logoImageSize.height * value.tshirtScale * newScale);
+                        final additionOffset = (previousImageSize - newImageSize) / 2;
+                        Offset newOffset = Offset.zero;
+                        if (newScale == value.logoScale) {
+                          newOffset = details.focalPoint - dragdifference + additionOffset;
+                        } else {
+                          newOffset = value.logoPositionOffset + additionOffset;
+                        }
+                        widget._controller.value = widget._controller.value.copyWith(
+                          logoScale: newScale,
+                          logoPositionOffset: newOffset,
+                        );
+                      },
+                      onScaleEnd: (details) {
+                        dragStarted = false;
+                        print('ScaleEnd');
+                      },
+                      child: Container(
+                        width: value.logoImageSize.width * value.tshirtScale * value.logoScale,
+                        height: value.logoImageSize.height * value.tshirtScale * value.logoScale,
+                        color: Colors.transparent,
+                      ),
+                    ),
+                  ),
           );
         });
   }
